ec77faeb8e96cd00c3f9985bd2c3cfb0ecf6eea6
barbaragncarvalho <barbarakawanyg@gmail.com>
2025-11-01 14:54:56 -0300

    chore: configurações .net e conexão com bd

---
+++ LeadGerenciamento.Api/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Lead> Leads { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+}
