1ffcf6efe785206cbf9dd7d786b384d91f367de0
barbaragncarvalho <barbarakawanyg@gmail.com>
2025-11-02 17:14:58 -0300

    chore: add cors para frontend react

--- LeadGerenciamento.Api/Program.cs
+++ LeadGerenciamento.Api/Program.cs
@@ -3,6 +3,16 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowReactApp",
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                      });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -25,6 +35,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowReactApp");
 
 app.UseAuthorization();
 
